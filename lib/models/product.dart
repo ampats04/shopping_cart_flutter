61d944e7245c700b1b40978b17b6a799093b4891
ampats04 <jeremyandyampatin@gmail.com>
2024-04-25 02:09:53 +0800

    feat(products): added product model

---
+++ lib/models/product.dart
@@ -0,0 +1,14 @@
+class Product {
+  final int id;
+  final String productName;
+  final String? productimage;
+  final String productDescription;
+  final double price;
+
+  Product(
+      {required this.id,
+      required this.productName,
+      this.productimage,
+      required this.productDescription,
+      required this.price});
+}
